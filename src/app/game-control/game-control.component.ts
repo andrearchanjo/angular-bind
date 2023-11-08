b4a213a3ebcd58c804c1aa480c2e85250670d025
André Coelho <archanjoandre@yahoo.com.br>
2023-11-08 16:27:38 -0300

    #master Função de iniciar e parar o jogo.

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent {
+  intervalCreated: any
+  numberCount: number = 0
 
+  onStartGame() {
+    this.intervalCreated = setInterval(() => {
+      this.numberCount++
+      console.log(this.numberCount)
+    }, 1000);
+  }
+
+  onStopGame() {
+    clearInterval(this.intervalCreated)
+  }
 }
